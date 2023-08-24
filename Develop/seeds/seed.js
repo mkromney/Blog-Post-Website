5708d5e40426a86b055c1d6739e3947749a8028f
Maridon Romney <mkromney@gmail.com>
2023-08-24 15:24:56 -0600

    added models, trying to test seeds

--- Develop/seeds/seed.js
+++ Develop/seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Blog } = require('../models');
+const { User, Post } = require('../models');
 
 const userData = require('./userData.json');
-const blogData = require('./blogData.json');
+const postData = require('./postData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +12,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const blog of blogData) {
-    await Blog.create({
-      ...blog,
+  for (const post of postData) {
+    await Post.create({
+      ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
